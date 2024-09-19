157024b6eaf7056c0172a1d94b0b6b048a136e78
Muhammad Bilal <bilaltheofficial@gmail.com>
2024-09-19 09:41:48 +0500

    remove extra spaces

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -8,7 +8,7 @@ const Meals = () => {
   useEffect(() => {
     async function fetchMeals() {
       const response = await fetch('http://localhost:3000/meals');
-      
+
       if (!response.ok) {
       }
       const meals = await response.json();
@@ -17,7 +17,7 @@ const Meals = () => {
 
     fetchMeals();
   }, [])
-  
+
 
   return (
     <ul id="meals">
