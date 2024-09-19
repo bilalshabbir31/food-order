8aba0d5905633068a89f2adf318f016652b96a6b
Muhammad Bilal <bilaltheofficial@gmail.com>
2024-09-19 21:29:22 +0500

    refactor checkout component

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -5,11 +5,22 @@ import { currencyFormatter } from "../util/formatting";
 import Input from "./UI/Input";
 import Button from "./UI/Button";
 import UserProgressContext from "./store/UserProgressContext";
+import useHttp from "../hooks/useHttp";
+import Error from "./Error";
+
+const requestConfig = {
+  method: "POST",
+  headers: {
+    'Content-Type': "application/json"
+  }
+};
 
 const Checkout = () => {
 
-  const { items } = useContext(CartContext);
+  const { items, clearCart } = useContext(CartContext);
   const { progress, hideCheckout } = useContext(UserProgressContext);
+
+  const { data, isLoading: isSending, error, sendRequest } = useHttp('http://localhost:3000/orders', requestConfig);
 
   const cartTotal = items.reduce((totalPrice, item) => totalPrice + item.quantity * item.price, 0)
 
@@ -19,15 +30,38 @@ const Checkout = () => {
     const fd = new FormData(event.target);
     const customerData = Object.fromEntries(fd.entries());
 
-    fetch('http://localhost:3000/orders', {method: 'POST', headers: {
-      'Content-Type': 'application/json'
-    }, body: JSON.stringify({
+    sendRequest(JSON.stringify({
       order: {
         items: items,
         customer: customerData
       }
-    })});
-    
+    }));
+  }
+
+  function handleFinished() {
+    hideCheckout();
+    clearCart();
+  }
+
+  let actions = (
+    <>
+      <Button type="button" textOnly onClick={hideCheckout}>Close</Button>
+      <Button>Submit Order</Button>
+    </>
+  )
+
+  if (isSending) {
+    actions = <span>Sending order data...</span>
+  }
+
+  if (data && !error) {
+    return <Modal open={progress === 'checkout'} onClose={hideCheckout}>
+      <h2>Sucess!</h2>
+      <p>Your order was submitted successfully.</p>
+      <p className="modal-actions">
+        <Button onClick={handleFinished}>Okay</Button>
+      </p>
+    </Modal>
   }
 
   return (
@@ -35,17 +69,17 @@ const Checkout = () => {
       <form onSubmit={handleSubmit}>
         <h2>Checkout</h2>
         <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
-        <Input label="Full Name" type='text' id="name"/>
+        <Input label="Full Name" type='text' id="name" />
         <Input label="Email Address" type='email' id="email" />
         <Input label="Street" type='text' id="street" />
         <div className="control-row">
           <Input label="Postal Code" type="text" id="postal-code" />
           <Input label="City" type='text' id="city" />
         </div>
-        <p className="modal-actions">
-          <Button type="button" textOnly onClick={hideCheckout}>Close</Button>
-          <Button>Submit Order</Button>
-        </p>
+
+        {error && <Error title="Failed to submit order" message={error} />}
+
+        <p className="modal-actions">{actions}</p>
       </form>
     </Modal>
   )
