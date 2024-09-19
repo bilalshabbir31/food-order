e8fe8d201daa8661c057d88810b4a4cd44324b4f
Muhammad Bilal <bilaltheofficial@gmail.com>
2024-09-19 16:42:15 +0500

    add handle submit method to checkout the cart

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -13,12 +13,29 @@ const Checkout = () => {
 
   const cartTotal = items.reduce((totalPrice, item) => totalPrice + item.quantity * item.price, 0)
 
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const fd = new FormData(event.target);
+    const customerData = Object.fromEntries(fd.entries());
+
+    fetch('http://localhost:3000/orders', {method: 'POST', headers: {
+      'Content-Type': 'application/json'
+    }, body: JSON.stringify({
+      order: {
+        items: items,
+        customer: customerData
+      }
+    })});
+    
+  }
+
   return (
     <Modal open={progress === 'checkout'} onClose={hideCheckout}>
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2>Checkout</h2>
         <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
-        <Input label="Full Name" type='text' id="full-name"/>
+        <Input label="Full Name" type='text' id="name"/>
         <Input label="Email Address" type='email' id="email" />
         <Input label="Street" type='text' id="street" />
         <div className="control-row">
